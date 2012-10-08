c849084f2a474e56455e9af633964c4726d47945
Vivek Haldar <vh@vivekhaldar.com>
2012-10-07 22:36:29 -0700

    Add NYT tech RSS.

--- subscriptions_print.py
+++ subscriptions_print.py
@@ -1,6 +1,7 @@
 # My RSS subscriptions, as a Python list.
 
 [
+    'http://feeds.nytimes.com/nyt/rss/Technology',
     'http://www.nytimes.com/services/xml/rss/nyt/GlobalHome.xml',
     'http://feeds.weatherbug.com/rss.aspx?zipcode=92617&feed=currtxt,fcsttxt&zcode=z4641',
 ]
